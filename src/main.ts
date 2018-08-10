549c2ad711627fa26636566dc22bcea40443455e
Leonnardo Verol <6124410+LeonnardoVerol@users.noreply.github.com>
2018-08-10 16:58:18 -0300

    Remove electron-reload

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { app, BrowserWindow, ipcMain } from "electron";
-require('electron-reload')(__dirname);
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
