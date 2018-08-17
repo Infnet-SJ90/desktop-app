65b290a46ac74da2ea7e8b916fd357ed480caf37
Calebe Santos <calebe.bsantos@outlook.com>
2018-08-16 22:24:10 -0300

    fix lint test

---
+++ test/pedido-agendamento.repository.test.ts
@@ -0,0 +1,15 @@
+import { assert, expect } from 'chai';
+import 'mocha';
+
+import PedidoDeAgendamento from '../src/pedido-agendamento';
+import PedidoDeAgendamentoRepository from '../src/pedido-agendamento.repository';
+
+describe('PedidoDeAgendamentoRepository', () => {
+
+	it('should return all Pedidos Mock', () => {
+		const expectResult: PedidoDeAgendamento[] = new PedidoDeAgendamentoRepository().getAll();
+		const result: PedidoDeAgendamento[] = new PedidoDeAgendamentoRepository().getAll();
+
+		expect(result).eql(expectResult);
+	});
+});
