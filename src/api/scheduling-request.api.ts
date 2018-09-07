4c911d29f4f64feb989597bcde6201dc2e1ecf86
Calebe Santos <calebe.bsantos@outlook.com>
2018-09-07 14:04:01 -0300

    extract data to json file

--- src/api/scheduling-request.api.ts
+++ src/api/scheduling-request.api.ts
@@ -1,37 +1,9 @@
+import getAllData from '../data/mock/scheduling-request.get-all.json';
 import SchedulingRequest from './../models/scheduling-request';
 
 class SchedulingRequestApi {
 	getAll(): SchedulingRequest[] {
-		const schedulingRequestList: SchedulingRequest[] = [];
-
-		let request = new SchedulingRequest();
-		request.id = 1;
-		request.citizen.name = 'Mark';
-		request.rubble.amount = 7;
-		request.distance = 4;
-		request.status = 0;
-		request.address.fullString = 'Rua São Jose';
-		schedulingRequestList.push(request);
-
-		request = new SchedulingRequest();
-		request.id = 2;
-		request.citizen.name = 'Jacob';
-		request.rubble.amount = 7;
-		request.distance = 5;
-		request.status = 0;
-		request.address.fullString = 'Rua São Jose';
-		schedulingRequestList.push(request);
-
-		request = new SchedulingRequest();
-		request.id = 3;
-		request.citizen.name = 'Larry';
-		request.rubble.amount = 7;
-		request.distance = 6;
-		request.status = 0;
-		request.address.fullString = 'Rua São Jose';
-		schedulingRequestList.push(request);
-
-		return schedulingRequestList;
+		return getAllData;
 	}
 }
 
