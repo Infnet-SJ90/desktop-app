247757a0e7bb8d1ef644e99c1f0c78aadce0ae57
Calebe Santos <calebe.bsantos@outlook.com>
2018-09-07 13:05:56 -0300

    extract to private home file

---
+++ src/scenes/private-home/private-home.renderer.ts
@@ -0,0 +1,25 @@
+import { ipcRenderer } from 'electron';
+
+import { ROUTER } from '../../routes';
+import { Renderer } from '../../utils/renderer';
+
+export class PrivateHomeRenderer implements Renderer {
+	private _schedulingRequestScene?: HTMLElement;
+	private _schedulingScene?: HTMLElement;
+
+	init() {
+		this._schedulingRequestScene = document.getElementById('btn-scheduling-request-list');
+		this._schedulingScene = document.getElementById('btn-scheduling-list');
+		if (this._schedulingRequestScene) {
+			this._schedulingRequestScene.addEventListener('click', () => {
+				ROUTER.navigate('/scheduling-request-list');
+			});
+		}
+
+		if (this._schedulingScene) {
+			this._schedulingScene.addEventListener('click', () => {
+				ipcRenderer.send('open-scheduling-list');
+			});
+		}
+	}
+}
