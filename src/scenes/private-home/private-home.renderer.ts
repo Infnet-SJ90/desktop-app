395f743c73213d9baa08c4d29348dbd19a51146b
Leonnardo Verol <6124410+LeonnardoVerol@users.noreply.github.com>
2018-09-17 21:47:22 -0300

    Fix Resource Scene Transition

--- src/scenes/private-home/private-home.renderer.ts
+++ src/scenes/private-home/private-home.renderer.ts
@@ -4,10 +4,13 @@ import { Renderer } from '../../utils/renderer';
 export class PrivateHomeRenderer implements Renderer {
 	private _schedulingRequestScene?: HTMLElement;
 	private _schedulingScene?: HTMLElement;
+	private _resourceCreationScene?: HTMLElement;
 
 	init() {
 		this._schedulingRequestScene = document.getElementById('btn-scheduling-request-list');
 		this._schedulingScene = document.getElementById('btn-scheduling-list');
+		this._resourceCreationScene = document.getElementById('btn-resource-creation');
+
 		if (this._schedulingRequestScene) {
 			this._schedulingRequestScene.addEventListener('click', () => {
 				ROUTER.navigate('/scheduling-request-list');
@@ -17,6 +20,12 @@ export class PrivateHomeRenderer implements Renderer {
 		if (this._schedulingScene) {
 			this._schedulingScene.addEventListener('click', () => {
 				ROUTER.navigate('/scheduling-list');
+			});
+		}
+
+		if (this._resourceCreationScene) {
+			this._resourceCreationScene.addEventListener('click', () => {
+				ROUTER.navigate('/resource-creation');
 			});
 		}
 	}
